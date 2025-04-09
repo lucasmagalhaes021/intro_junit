304da648ef8e8a7828a3184a008d89d205936607
Lucas Magalhães <lucasferreiramagalhaes@outlook.com>
2025-04-09 19:43:53 -0300

    Arquivos de IDLE

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    
+    public double calc(double a,
+                       double b,
+                       char method) throws IllegalArgumentException {
+        
+        if (method == '-') {
+            return a - b;
+        } else if (method == '+') {
+            return a + b;
+        } else if (method == '*') {
+            return a * b;
+        } else if (method == '/') {
+            return a / b;
+        } else {
+            throw new IllegalArgumentException("Invalid operation"); 
+        }
+
+    }
+
+}
